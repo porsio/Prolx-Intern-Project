5a81b7bcca99216250707c07453b465e2e43f613
Pranav Santhosh <59979143+npv12@users.noreply.github.com>
2020-08-15 15:39:22 +0530

    complete Expanded Product widget

--- lib/Widget/ProductExpanded.dart
+++ lib/Widget/ProductExpanded.dart
@@ -43,12 +43,56 @@ class BuildExpandedPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: [
-          Column(
-            children: [Text("PERSON 1"), Text("Person 2")],
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                TextField(text: "Person 1"),
+                TextField(text: "Person 2"),
+                TextField(text: "Person 3"),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(text: "Date Expires on "),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                TextField(text: "100"),
+                TextField(text: "100"),
+                TextField(text: "100"),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: FloatingActionButton(
+                    onPressed: null,
+                    child: Icon(Icons.edit),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextField extends StatelessWidget {
+  final String text;
+  const TextField({
+    Key key,
+    @required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 18),
       ),
     );
   }
